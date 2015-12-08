cb3b7e7c26af3632a45d1a41a76010f939d46810
unknown <soapmctravich@gmail.com>
2015-12-08 16:50:19 +0100

    Creation de nouvelles salles

--- app/scripts/controllers/accueil.js
+++ app/scripts/controllers/accueil.js
@@ -12,4 +12,14 @@ angular.module('b3AgileCalendarApp')
   $scope.goTo = function(room){
     $location.path('/' + room);
   }
+
+  $scope.createRoom = function(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 8; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    $location.path('/' + text);
+  }
 });
